8536a3a7b243be29815433fa4e4babb3c8dde4b7
AI-Scout <131296521+AI-Scout@users.noreply.github.com>
2024-07-19 14:32:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 import streamlit as st
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
-# @st.cache_resource
+@st.cache_resource
 def get_driver():
     return webdriver.Chrome(
         service=Service(
@@ -20,6 +19,9 @@ options.add_argument("--disable-gpu")
 options.add_argument("--headless")
 
 driver = get_driver()
-driver.get("http://apple.com")
 
-st.code(driver.page_source)
+def page_source(url):
+    driver.get(url)
+    return driver.page_source
+
+st.code(page_source("http://apple.com"))
