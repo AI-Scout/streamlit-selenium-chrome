255bc1e17a6648b2eb305acf74be6747277f298c
AI-Scout <131296521+AI-Scout@users.noreply.github.com>
2024-07-19 22:35:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def get_driver():
 
 options = Options()
 # options.add_argument("--disable-gpu")
-# options.add_argument("--headless")
+options.add_argument("--headless")
 
 driver = get_driver()
 
