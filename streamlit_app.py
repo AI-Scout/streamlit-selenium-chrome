f442e41f75f4351274d489559195505d0bbed389
AI-Scout <131296521+AI-Scout@users.noreply.github.com>
2024-07-19 22:05:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,10 +29,8 @@ def get_urls(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     return [a['href'] for a in soup.find_all('a', href=True)]
 
-urls = get_urls(page_source("http://apple.com"))
+urls = get_urls(page_source("http://example.com"))
 
 for product_url in urls:
     st.markdown(f'<a href="{product_url}" download target="_blank">{product_url}</a>', unsafe_allow_html=True)
     
-st.code(page_source("http://apple.com"))
-
